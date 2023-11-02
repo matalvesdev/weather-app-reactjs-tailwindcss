b9f9b34f905e56c9effd2a5cd1bc6da4bc907aac
Mateus Alves <89415601+matalvesdev@users.noreply.github.com>
2023-11-02 20:10:58 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html', // Caminho para arquivos HTML que usam classes Tailwind CSS
+    './src/**/*.js',   // Caminho para arquivos JavaScript que usam classes Tailwind CSS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
